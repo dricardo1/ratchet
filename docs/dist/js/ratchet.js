f9cbd12a71ec819363b0decd488608192215413d
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-24 08:31:38 +0200

    Run `grunt`.

--- docs/dist/js/ratchet.js
+++ docs/dist/js/ratchet.js
@@ -15,7 +15,7 @@
  * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
  * ======================================================================== */
 
-!(function () {
+(function () {
   'use strict';
 
   var findModals = function (target) {
@@ -47,7 +47,7 @@
       event.preventDefault(); // prevents rewriting url (apps can still use hash values in url)
     }
   });
-}());
+})();
 
 /* ========================================================================
  * Ratchet: popovers.js v2.0.2
@@ -57,7 +57,7 @@
  * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
  * ======================================================================== */
 
-!(function () {
+(function () {
   'use strict';
 
   var popover;
@@ -103,8 +103,7 @@
 
     try {
       popover = document.querySelector(anchor.hash);
-    }
-    catch (error) {
+    } catch (error) {
       popover = null;
     }
 
@@ -127,7 +126,7 @@
     }
 
     popover.style.display = 'block';
-    popover.offsetHeight;
+    popover.offsetHeight; // jshint ignore:line
     popover.classList.add('visible');
 
     popover.parentNode.appendChild(backdrop);
@@ -135,7 +134,7 @@
 
   window.addEventListener('touchend', showHidePopover);
 
-}());
+})();
 
 /* ========================================================================
  * Ratchet: push.js v2.0.2
@@ -148,7 +147,7 @@
 
 /* global _gaq: true */
 
-!(function () {
+(function () {
   'use strict';
 
   var noop = function () {};
@@ -335,7 +334,8 @@
 
     PUSH.id = id;
 
-    document.body.offsetHeight; // force reflow to prevent scroll
+    // force reflow to prevent scroll
+    document.body.offsetHeight; // jshint ignore:line
   };
 
 
@@ -368,7 +368,11 @@
         clearTimeout(options._timeout);
       }
       if (xhr.readyState === 4) {
-        xhr.status === 200 ? success(xhr, options) : failure(options.url);
+        if (xhr.status === 200) {
+          success(xhr, options);
+        } else {
+          failure(options.url);
+        }
       }
     };
 
@@ -383,7 +387,9 @@
     }
 
     if (options.timeout) {
-      options._timeout = setTimeout(function () {  xhr.abort('timeout'); }, options.timeout);
+      options._timeout = setTimeout(function () {
+        xhr.abort('timeout');
+      }, options.timeout);
     }
 
     xhr.send();
@@ -464,7 +470,7 @@
         document.body.insertBefore(swap, document.querySelector('.content'));
       }
     } else {
-      enter  = /in$/.test(transition);
+      enter = /in$/.test(transition);
 
       if (transition === 'fade') {
         container.classList.add('in');
@@ -482,11 +488,14 @@
     }
 
     if (!transition) {
-      complete && complete();
+      if (complete) {
+        complete();
+      }
     }
 
     if (transition === 'fade') {
-      container.offsetWidth; // force reflow
+      // force reflow
+      container.offsetWidth; // jshint ignore:line
       container.classList.remove('in');
       var fadeContainerEnd = function () {
         container.removeEventListener('webkitTransitionEnd', fadeContainerEnd);
@@ -498,7 +507,9 @@
         container.parentNode.removeChild(container);
         swap.classList.remove('fade');
         swap.classList.remove('in');
-        complete && complete();
+        if (complete) {
+          complete();
+        }
       };
       container.addEventListener('webkitTransitionEnd', fadeContainerEnd);
 
@@ -510,10 +521,13 @@
         swap.classList.remove('sliding', 'sliding-in');
         swap.classList.remove(swapDirection);
         container.parentNode.removeChild(container);
-        complete && complete();
+        if (complete) {
+          complete();
+        }
       };
 
-      container.offsetWidth; // force reflow
+      // force reflow
+      container.offsetWidth; // jshint ignore:line
       swapDirection      = enter ? 'right' : 'left';
       containerDirection = enter ? 'left' : 'right';
       container.classList.add(containerDirection);
@@ -524,7 +538,9 @@
 
   var triggerStateChange = function () {
     var e = new CustomEvent('push', {
-      detail: { state: getCached(PUSH.id) },
+      detail: {
+        state: getCached(PUSH.id)
+      },
       bubbles: true,
       cancelable: true
     });
@@ -612,14 +628,22 @@
   // Attach PUSH event handlers
   // ==========================
 
-  window.addEventListener('touchstart', function () { isScrolling = false; });
-  window.addEventListener('touchmove', function () { isScrolling = true; });
+  window.addEventListener('touchstart', function () {
+    isScrolling = false;
+  });
+  window.addEventListener('touchmove', function () {
+    isScrolling = true;
+  });
   window.addEventListener('touchend', touchend);
-  window.addEventListener('click', function (e) { if (getTarget(e)) {e.preventDefault();} });
+  window.addEventListener('click', function (e) {
+    if (getTarget(e)) {
+      e.preventDefault();
+    }
+  });
   window.addEventListener('popstate', popstate);
   window.PUSH = PUSH;
 
-}());
+})();
 
 /* ========================================================================
  * Ratchet: segmented-controllers.js v2.0.2
@@ -629,7 +653,7 @@
  * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
  * ======================================================================== */
 
-!(function () {
+(function () {
   'use strict';
 
   var getTarget = function (target) {
@@ -684,8 +708,13 @@
     targetBody.classList.add(className);
   });
 
-  window.addEventListener('click', function (e) { if (getTarget(e.target)) {e.preventDefault();} });
-}());
+  window.addEventListener('click', function (e) {
+    if (getTarget(e.target)) {
+      e.preventDefault();
+    }
+  });
+
+})();
 
 /* ========================================================================
  * Ratchet: sliders.js v2.0.2
@@ -696,7 +725,7 @@
  * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
  * ======================================================================== */
 
-!(function () {
+(function () {
   'use strict';
 
   var pageX;
@@ -800,9 +829,7 @@
       return;
     }
 
-    setSlideNumber(
-      (+new Date()) - startTime < 1000 && Math.abs(deltaX) > 15 ? (deltaX < 0 ? -1 : 1) : 0
-    );
+    setSlideNumber((+new Date()) - startTime < 1000 && Math.abs(deltaX) > 15 ? (deltaX < 0 ? -1 : 1) : 0);
 
     offsetX = slideNumber * sliderWidth;
 
@@ -810,7 +837,9 @@
     slider.style.webkitTransform = 'translate3d(' + offsetX + 'px,0,0)';
 
     e = new CustomEvent('slide', {
-      detail: { slideNumber: Math.abs(slideNumber) },
+      detail: {
+        slideNumber: Math.abs(slideNumber)
+      },
       bubbles: true,
       cancelable: true
     });
@@ -822,7 +851,7 @@
   window.addEventListener('touchmove', onTouchMove);
   window.addEventListener('touchend', onTouchEnd);
 
-}());
+})();
 
 /* ========================================================================
  * Ratchet: toggles.js v2.0.2
@@ -833,7 +862,7 @@
  * Licensed under MIT (https://github.com/twbs/ratchet/blob/master/LICENSE)
  * ======================================================================== */
 
-!(function () {
+(function () {
   'use strict';
 
   var start     = {};
@@ -868,7 +897,10 @@
     var handleWidth = handle.clientWidth;
     var offset      = toggle.classList.contains('active') ? (toggleWidth - handleWidth) : 0;
 
-    start     = { pageX : e.touches[0].pageX - offset, pageY : e.touches[0].pageY };
+    start = {
+      pageX: e.touches[0].pageX - offset,
+      pageY: e.touches[0].pageY
+    };
     touchMove = false;
   });
 
@@ -930,7 +962,9 @@
     toggle.classList[slideOn ? 'add' : 'remove']('active');
 
     e = new CustomEvent('toggle', {
-      detail: { isActive: slideOn },
+      detail: {
+        isActive: slideOn
+      },
       bubbles: true,
       cancelable: true
     });
@@ -941,4 +975,4 @@
     toggle    = false;
   });
 
-}());
+})();
